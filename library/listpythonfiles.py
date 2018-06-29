80fa24a0f969572f9ed4a1fe9234a8ecfb1d8bf5
srikanthpragada <srikanthpragada@gmail.com>
2018-06-29 08:30:49 +0530

    29-JUNE

---
+++ library/listpythonfiles.py
@@ -0,0 +1,14 @@
+
+import os
+
+files = os.walk(r"e:\classroom\python\june11\demo")
+
+for  (dn,dl,fl) in files:
+    if dn.find(".git") >= 0:
+        continue
+    print("Directory : ", dn)
+    print("Files %d, directories %d" % (len(fl), len(dl)))
+    for f in fl:
+        if f.endswith(".py"):
+            print(f)
+
